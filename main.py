2b28d9c199e3937c7b6ec69fd486b8014b7db174
Enrique Gongora <EGTech24@gmail.com>
2021-01-27 15:50:16 -0800

    Created the Paddle

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from turtle import Turtle, Screen
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
+
+def up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 screen.exitonclick()
