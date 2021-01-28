e764c058ba12784b907d72a1264ce8bcd350c687
Enrique Gongora <EGTech24@gmail.com>
2021-01-27 17:52:09 -0800

    Implemented ball logic so it changes directions when it collides with the top wall

--- main.py
+++ main.py
@@ -21,8 +21,11 @@ screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
     screen.update()
+    time.sleep(0.1)
     ball.move()
+
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        ball.bounce()
 
 screen.exitonclick()
